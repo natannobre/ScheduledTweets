81716fd8a31b5ca0856ea493808539115fd5129f
natannobre <natannobre37@gmail.com>
2021-08-24 09:37:10 -0300

    Handling Sign Up Errors

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,21 @@ class RegistrationsController < ApplicationController
     def new
         @user = User.new
     end
-end
+
+    def create 
+        # render plain: "Thanks" # Rendering a text and sanding backs
+        # render plain: params[:user] # This print out tha params hash, but just the portion corresponding to users
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Successfuly created account"
+        else
+            render :new # :new will render views/registrations/new.html.erb
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@
 # password:string virtual
 # password_confirmation:string virtual
 class User < ApplicationRecord
-    has_secure_password
+    has_secure_password # add a confirmation for the password
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "must be a valid email address" }
 end
