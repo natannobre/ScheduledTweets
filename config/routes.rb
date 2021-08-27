49bcb4beb73ee737eace0cb47487c440297608d2
natannobre <natannobre37@gmail.com>
2021-08-27 11:59:51 -0300

    Login Form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-   # root route
-   # another way would be: get "/", to: "main#index", as: :root
-   root to: "main#index"
+  # root route
+  # another way would be: get "/", to: "main#index", as: :root
+  root to: "main#index"
 
   # Get /about
   get "/about", to: "about#index", as: :about
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   # Get /sign_up
   get "sign_up", to: "registrations#new", as: :sign_up
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
   delete "logout", to: "sessions#destroy"
 end
