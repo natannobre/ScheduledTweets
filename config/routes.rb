303a41b11398759dc8b450c1351a4bf9b3cdb501
natannobre <natannobre37@gmail.com>
2021-10-27 21:21:30 -0300

    Forgot Your Password

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
 
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+
   # Get /sign_up
   get "sign_up", to: "registrations#new", as: :sign_up
   post "sign_up", to: "registrations#create"
